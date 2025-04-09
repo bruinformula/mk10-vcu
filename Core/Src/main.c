6a465fd4f88384de3fa65b900581b764de4795b9
omid <omidjanani110@gmail.com>
2025-04-09 00:46:23 -0700

    working .wav playback

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -2,58 +2,70 @@
 /**
   ******************************************************************************
   * @file           : main.c
-  * @brief          : Main program body
+  * @brief          : Plays entire WAV exactly once in Normal DMA, chunk-based
   ******************************************************************************
   * @attention
   *
-  * Copyright (c) 2025 STMicroelectronics.
-  * All rights reserved.
-  *
-  * This software is licensed under terms that can be found in the LICENSE file
-  * in the root directory of this software component.
-  * If no LICENSE file comes with this software, it is provided AS-IS.
+  * This code ensures no repeats and no truncation,
+  * even if the WAV is bigger than 65535 halfwords.
+  * It splits the wave into consecutive DMA transmissions.
   *
   ******************************************************************************
   */
 /* USER CODE END Header */
-
 /* Includes ------------------------------------------------------------------*/
 #include "main.h"
 
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
-#include "startup_sound.h" // <-- Use your new startup_sound data
-#include <math.h>
+#include "startup_sound.h" // Must have const unsigned char startup_sound[]
+#include <string.h>        // for memset if needed
 /* USER CODE END Includes */
 
 /* Private typedef -----------------------------------------------------------*/
 /* USER CODE BEGIN PTD */
-
 /* USER CODE END PTD */
 
 /* Private define ------------------------------------------------------------*/
 /* USER CODE BEGIN PD */
 
-// Typically a .wav has a 44-byte header for 16-bit PCM stereo, 44.1 kHz:
-#define WAV_HEADER_SIZE  44
+// WAV header is typically 44 bytes for 16-bit stereo PCM
+#define WAV_HEADER_SIZE       44
+// total PCM bytes
+#define WAV_DATA_SIZE         (startup_sound_len - WAV_HEADER_SIZE)
+// total halfwords in the PCM
+#define TOTAL_HALFWORDS       (WAV_DATA_SIZE / 2)
 
-// The raw PCM size (file size minus header).
-// Must ensure startup_sound_len >= 44:
-#define WAV_DATA_SIZE    (startup_sound_len - WAV_HEADER_SIZE)
+// We'll transmit chunks of up to e.g. 30000 halfwords
+// to avoid the 65535 limit in a single call
+#define CHUNK_SIZE_HALFWORDS  30000
 
 /* USER CODE END PD */
 
 /* Private macro -------------------------------------------------------------*/
 /* USER CODE BEGIN PM */
-
 /* USER CODE END PM */
 
 /* Private variables ---------------------------------------------------------*/
 I2C_HandleTypeDef hi2c1;
+
 I2S_HandleTypeDef hi2s2;
 DMA_HandleTypeDef hdma_spi2_tx;
 
 /* USER CODE BEGIN PV */
+
+// Global position in halfwords into the wave
+static uint32_t wavPos = 0;
+
+// We'll point to the PCM data (16-bit samples, skipping 44-byte header)
+static const uint16_t* wavePCM = NULL;
+
+// total halfwords in wave
+static uint32_t halfwordCount = 0;
+
+// We'll track if we've fully played the wave
+static uint8_t waveFinished = 0;
+
 /* USER CODE END PV */
 
 /* Private function prototypes -----------------------------------------------*/
@@ -63,11 +75,59 @@ static void MX_DMA_Init(void);
 static void MX_I2C1_Init(void);
 static void MX_I2S2_Init(void);
 /* USER CODE BEGIN PFP */
+/*
+   We'll define a helper function to start the next chunk in Normal DMA mode.
+*/
+static void StartNextChunk(void);
 /* USER CODE END PFP */
 
 /* Private user code ---------------------------------------------------------*/
 /* USER CODE BEGIN 0 */
-// No extra code here.
+
+/**
+  * @brief  Called when a DMA transmission is complete (one chunk).
+  */
+void HAL_I2S_TxCpltCallback(I2S_HandleTypeDef *hi2s)
+{
+    if (hi2s->Instance == SPI2 && !waveFinished)
+    {
+        // We finished one chunk
+        if (wavPos < halfwordCount)
+        {
+            // More data left => start next chunk
+            StartNextChunk();
+        }
+        else
+        {
+            // All data was sent
+            waveFinished = 1;
+        }
+    }
+}
+
+/**
+  * @brief  Start the next chunk of PCM in Normal DMA mode
+  */
+static void StartNextChunk(void)
+{
+    // how many halfwords remain?
+    uint32_t remain = halfwordCount - wavPos;
+    // pick chunk size
+    uint16_t thisChunk = (remain > CHUNK_SIZE_HALFWORDS)
+                         ? CHUNK_SIZE_HALFWORDS
+                         : (uint16_t)remain;
+
+    // pointer to next data
+    const uint16_t* chunkPtr = wavePCM + wavPos;
+
+    // update position
+    wavPos += thisChunk;
+
+    // Kick off the DMA
+    // After it finishes, HAL_I2S_TxCpltCallback is called
+    HAL_I2S_Transmit_DMA(&hi2s2, (uint16_t*)chunkPtr, thisChunk);
+}
+
 /* USER CODE END 0 */
 
 /**
@@ -76,39 +136,52 @@ static void MX_I2S2_Init(void);
   */
 int main(void)
 {
+
+  /* USER CODE BEGIN 1 */
+
+  /* USER CODE END 1 */
+
   /* MCU Configuration--------------------------------------------------------*/
+
+  /* Reset of all peripherals, Initializes the Flash interface and the Systick. */
   HAL_Init();
+
+  /* USER CODE BEGIN Init */
+
+  /* USER CODE END Init */
+
+  /* Configure the system clock */
   SystemClock_Config();
+
+  /* USER CODE BEGIN SysInit */
+
+  /* USER CODE END SysInit */
 
   /* Initialize all configured peripherals */
   MX_GPIO_Init();
   MX_DMA_Init();
   MX_I2C1_Init();
   MX_I2S2_Init();
-
   /* USER CODE BEGIN 2 */
-  // 1) Pointer to raw PCM data, skipping the 44-byte WAV header:
-  const uint16_t *audioStart = (const uint16_t *)&startup_sound[WAV_HEADER_SIZE];
+  wavePCM       = (const uint16_t*)&startup_sound[WAV_HEADER_SIZE];
+  halfwordCount = TOTAL_HALFWORDS;
+  wavPos        = 0;
+  waveFinished  = 0;
 
-  // 2) total halfwords = total bytes / 2
-  uint32_t halfwordCount = WAV_DATA_SIZE / 2;
-
-  // 3) Play the WAV file once via DMA
-  HAL_I2S_Transmit_DMA(&hi2s2, (uint16_t *)audioStart, halfwordCount);
-
+  // Start the first chunk
+  StartNextChunk();
   /* USER CODE END 2 */
 
   /* Infinite loop */
   /* USER CODE BEGIN WHILE */
   while (1)
   {
-    // The I2S peripheral + DMA will transmit in background.
-    // Once the file is finished, the sound stops.
+    /* USER CODE END WHILE */
+
+    /* USER CODE BEGIN 3 */
   }
-  /* USER CODE END WHILE */
-  /* USER CODE BEGIN 3 */
+  /* USER CODE END 3 */
 }
-/* USER CODE END 3 */
 
 /**
   * @brief System Clock Configuration
@@ -132,12 +205,13 @@ void SystemClock_Config(void)
   __HAL_RCC_PWR_CLK_ENABLE();
   __HAL_PWR_VOLTAGESCALING_CONFIG(PWR_REGULATOR_VOLTAGE_SCALE2);
 
-  /** Initializes the RCC Oscillators
+  /** Initializes the RCC Oscillators according to the specified parameters
+  * in the RCC_OscInitTypeDef structure.
   */
   RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI;
-  RCC_OscInitStruct.HSIState       = RCC_HSI_ON;
+  RCC_OscInitStruct.HSIState = RCC_HSI_ON;
   RCC_OscInitStruct.HSICalibrationValue = RCC_HSICALIBRATION_DEFAULT;
-  RCC_OscInitStruct.PLL.PLLState  = RCC_PLL_NONE;
+  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_NONE;
   RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSI;
   if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
   {
@@ -146,12 +220,10 @@ void SystemClock_Config(void)
 
   /** Initializes the CPU, AHB and APB buses clocks
   */
-  RCC_ClkInitStruct.ClockType      = RCC_CLOCKTYPE_HCLK
-                                   | RCC_CLOCKTYPE_SYSCLK
-                                   | RCC_CLOCKTYPE_PCLK1
-                                   | RCC_CLOCKTYPE_PCLK2;
-  RCC_ClkInitStruct.SYSCLKSource   = RCC_SYSCLKSOURCE_HSI;  // ~16MHz
-  RCC_ClkInitStruct.AHBCLKDivider  = RCC_SYSCLK_DIV1;
+  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
+                              |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
+  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_HSI;
+  RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
   RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV1;
   RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
 
@@ -163,42 +235,70 @@ void SystemClock_Config(void)
 
 /**
   * @brief I2C1 Initialization Function
+  * @param None
+  * @retval None
   */
 static void MX_I2C1_Init(void)
 {
-  hi2c1.Instance             = I2C1;
-  hi2c1.Init.ClockSpeed      = 100000;
-  hi2c1.Init.DutyCycle       = I2C_DUTYCYCLE_2;
-  hi2c1.Init.OwnAddress1     = 0;
-  hi2c1.Init.AddressingMode  = I2C_ADDRESSINGMODE_7BIT;
+
+  /* USER CODE BEGIN I2C1_Init 0 */
+
+  /* USER CODE END I2C1_Init 0 */
+
+  /* USER CODE BEGIN I2C1_Init 1 */
+
+  /* USER CODE END I2C1_Init 1 */
+  hi2c1.Instance = I2C1;
+  hi2c1.Init.ClockSpeed = 100000;
+  hi2c1.Init.DutyCycle = I2C_DUTYCYCLE_2;
+  hi2c1.Init.OwnAddress1 = 0;
+  hi2c1.Init.AddressingMode = I2C_ADDRESSINGMODE_7BIT;
   hi2c1.Init.DualAddressMode = I2C_DUALADDRESS_DISABLE;
-  hi2c1.Init.OwnAddress2     = 0;
+  hi2c1.Init.OwnAddress2 = 0;
   hi2c1.Init.GeneralCallMode = I2C_GENERALCALL_DISABLE;
-  hi2c1.Init.NoStretchMode   = I2C_NOSTRETCH_DISABLE;
+  hi2c1.Init.NoStretchMode = I2C_NOSTRETCH_DISABLE;
   if (HAL_I2C_Init(&hi2c1) != HAL_OK)
   {
     Error_Handler();
   }
+  /* USER CODE BEGIN I2C1_Init 2 */
+
+  /* USER CODE END I2C1_Init 2 */
+
 }
 
 /**
   * @brief I2S2 Initialization Function
+  * @param None
+  * @retval None
   */
 static void MX_I2S2_Init(void)
 {
-  hi2s2.Instance         = SPI2;
-  hi2s2.Init.Mode        = I2S_MODE_MASTER_TX;
-  hi2s2.Init.Standard    = I2S_STANDARD_PHILIPS;
-  hi2s2.Init.DataFormat  = I2S_DATAFORMAT_16B;
-  hi2s2.Init.MCLKOutput  = I2S_MCLKOUTPUT_DISABLE;
-  hi2s2.Init.AudioFreq   = I2S_AUDIOFREQ_44K; // ~44.1 kHz
-  hi2s2.Init.CPOL        = I2S_CPOL_LOW;
+
+  /* USER CODE BEGIN I2S2_Init 0 */
+
+  /* USER CODE END I2S2_Init 0 */
+
+  /* USER CODE BEGIN I2S2_Init 1 */
+
+  /* USER CODE END I2S2_Init 1 */
+  hi2s2.Instance = SPI2;
+  hi2s2.Init.Mode = I2S_MODE_MASTER_TX;
+  hi2s2.Init.Standard = I2S_STANDARD_PHILIPS;
+  hi2s2.Init.DataFormat = I2S_DATAFORMAT_16B;
+  hi2s2.Init.MCLKOutput = I2S_MCLKOUTPUT_DISABLE;
+  hi2s2.Init.AudioFreq = I2S_AUDIOFREQ_44K;
+  hi2s2.Init.CPOL = I2S_CPOL_LOW;
   hi2s2.Init.ClockSource = I2S_CLOCK_PLL;
   hi2s2.Init.FullDuplexMode = I2S_FULLDUPLEXMODE_DISABLE;
   if (HAL_I2S_Init(&hi2s2) != HAL_OK)
   {
     Error_Handler();
   }
+  /* USER CODE BEGIN I2S2_Init 2 */
+
+  /* USER CODE END I2S2_Init 2 */
+
 }
 
 /**
@@ -206,6 +306,7 @@ static void MX_I2S2_Init(void)
   */
 static void MX_DMA_Init(void)
 {
+
   /* DMA controller clock enable */
   __HAL_RCC_DMA1_CLK_ENABLE();
 
@@ -213,40 +314,69 @@ static void MX_DMA_Init(void)
   /* DMA1_Stream4_IRQn interrupt configuration */
   HAL_NVIC_SetPriority(DMA1_Stream4_IRQn, 0, 0);
   HAL_NVIC_EnableIRQ(DMA1_Stream4_IRQn);
+
 }
 
 /**
   * @brief GPIO Initialization Function
+  * @param None
+  * @retval None
   */
 static void MX_GPIO_Init(void)
 {
   GPIO_InitTypeDef GPIO_InitStruct = {0};
+/* USER CODE BEGIN MX_GPIO_Init_1 */
+/* USER CODE END MX_GPIO_Init_1 */
 
   /* GPIO Ports Clock Enable */
   __HAL_RCC_GPIOH_CLK_ENABLE();
   __HAL_RCC_GPIOB_CLK_ENABLE();
   __HAL_RCC_GPIOA_CLK_ENABLE();
 
-  // Example for PA15 if needed for some SPI usage
-  GPIO_InitStruct.Pin       = GPIO_PIN_15;
-  GPIO_InitStruct.Mode      = GPIO_MODE_AF_PP;
-  GPIO_InitStruct.Pull      = GPIO_NOPULL;
-  GPIO_InitStruct.Speed     = GPIO_SPEED_FREQ_LOW;
+  /*Configure GPIO pin : PA15 */
+  GPIO_InitStruct.Pin = GPIO_PIN_15;
+  GPIO_InitStruct.Mode = GPIO_MODE_AF_PP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
   GPIO_InitStruct.Alternate = GPIO_AF6_SPI3;
   HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
+
+/* USER CODE BEGIN MX_GPIO_Init_2 */
+/* USER CODE END MX_GPIO_Init_2 */
 }
 
+/* USER CODE BEGIN 4 */
+
+/* USER CODE END 4 */
+
+/**
+  * @brief  This function is executed in case of error occurrence.
+  * @retval None
+  */
 void Error_Handler(void)
 {
+  /* USER CODE BEGIN Error_Handler_Debug */
+  /* User can add his own implementation to report the HAL error return state */
   __disable_irq();
   while (1)
   {
-    // Stuck here on error
   }
+  /* USER CODE END Error_Handler_Debug */
 }
 
 #ifdef  USE_FULL_ASSERT
+/**
+  * @brief  Reports the name of the source file and the source line number
+  *         where the assert_param error has occurred.
+  * @param  file: pointer to the source file name
+  * @param  line: assert_param error line source number
+  * @retval None
+  */
 void assert_failed(uint8_t *file, uint32_t line)
 {
+  /* USER CODE BEGIN 6 */
+  /* User can add his own implementation to report the file name and line number,
+     ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
+  /* USER CODE END 6 */
 }
-#endif
+#endif /* USE_FULL_ASSERT */
