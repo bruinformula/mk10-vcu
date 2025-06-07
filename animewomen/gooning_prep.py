f8669f12d2eff31e437fd983420133d7f877b82c
omid <omidjanani110@gmail.com>
2025-06-06 22:35:32 -0700

    i goon to yashers_b

--- animewomen/gooning_prep.py
+++ animewomen/gooning_prep.py
@@ -1,13 +1,13 @@
 import numpy
 
-goon_list = {"yashers_b", "lony_tang", "bassim_cs"}
+goon_list = {"yashers_b", "lony_tang", "bassim_cs", "kadin_m", "randih"}
 
-fout = open("goon_list.txt", "w")
+fout = open("goon_list2.txt", "w")
 
 for each in goon_list:
     print ("I love " + each + "!!!!")
     
-for i in range (5000):
+for i in range (20000):
     for each in goon_list:
         fout.write("I F*CKING LOVE " + each + "!!!!!!!!!!\n")
         print(i)
